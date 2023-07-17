71d0e928a5085ef8ba8f5d6a90600a21901e9854
Loup <yakarycr@gmail.com>
2023-07-17 12:50:30 -0400

    e

--- src/world_events.c
+++ src/world_events.c
@@ -5,26 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yakary <yakary@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/14 15:33:48 by ycyr-roy          #+#    #+#             */
-/*   Updated: 2023/07/15 01:24:43 by yakary           ###   ########.fr       */
+/*   Created: 2023/07/15 09:34:53 by yakary            #+#    #+#             */
+/*   Updated: 2023/07/15 09:35:38 by yakary           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 
-static void zombie_move(t_data *data)
-{
-    t_co co;
+// static void zombie_move(t_data *data)
+// {
+//     t_co co;
 
-    co = where_is(0, PLAYER, data->map);
-    printf("FOUND P IN (%d,%d)\n", co.x, co.y);
-    flood_fill(data, co.x, co.y, 1);
-    printf("Distance of %d between zombie and player\n", data->distance_map[co.y][co.x] - 1);
+//     co = where_is(0, PLAYER, data->map);
+//     printf("FOUND P IN (%d,%d)\n", co.x, co.y);
+//     flood_fill(data, co.x, co.y, 1);
+//     printf("Distance of %d between zombie and player\n", data->distance_map[co.y][co.x] - 1);
     
-}
+// }
 
-void    world_events(t_data *data)
-{
-    zombie_move(data);
-}
+// void    world_events(t_data *data)
+// {
+//     zombie_move(data);
+// }
